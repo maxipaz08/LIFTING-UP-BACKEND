714a955df6ab504bf39cd11a8f8a7a752ee28dd5
maxipaz08 <pazmax563@gmail.com>
2026-09-03 14:39:27 -0300

    Actualiza backend sin node_modules

--- Server.js
+++ Server.js
@@ -17,10 +17,16 @@ app.use(express.json());
 // Importar Rutas
 const usuariosRoutes = require('./routes/usuarios');
 const adminsRoutes = require('./routes/adminsRoutes');
+const rutinasRoutes = require('./routes/rutinas');
+const asistenciaRoutes = require('./routes/asistencia');
+const ejerciciosRoutes = require('./routes/ejercicios');
 
 // Rutas base
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/admins', adminsRoutes);
+app.use('/api/rutinas', rutinasRoutes);
+app.use('/api/asistencia', asistenciaRoutes);
+app.use('/api/ejercicios', ejerciciosRoutes);
 
 app.get('/', (req, res) => {
     res.send('Servidor API LIFTING UP funcionando correctamente');
